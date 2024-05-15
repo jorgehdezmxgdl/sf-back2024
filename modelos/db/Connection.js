a07f74033dbd218a85f5025e91d53892ce6956f4
jorgehdezmxgdl <67309059+jorgehdezmxgdl@users.noreply.github.com>
2024-05-15 14:27:34 -0600

    Eliminar espacios

--- modelos/db/Connection.js
+++ modelos/db/Connection.js
@@ -2,11 +2,8 @@
 const { Sequelize } = require('sequelize');
 
 const sequelize  = new Sequelize('safav3', 'root', '12345678', {
-<<<<<<< Updated upstream
   host: '127.0.0.1',
-=======
   host: 'localhost',
->>>>>>> Stashed changes
   dialect: 'mysql',  
   define: {
     timestamps: true,
