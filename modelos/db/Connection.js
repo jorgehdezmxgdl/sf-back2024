63a014761f47df315502f6028625451f48b8bddc
jorgehdezmxgdl <67309059+jorgehdezmxgdl@users.noreply.github.com>
2024-06-10 22:43:42 -0600

    COMPRAS

--- modelos/db/Connection.js
+++ modelos/db/Connection.js
@@ -1,7 +1,7 @@
 // sequelize-auto -h 127.0.0.1 -x -d safav3 -u root -o modelos -cm p --sg 
 const { Sequelize } = require('sequelize');
 
-const sequelize  = new Sequelize('safav3', 'root', '12345', {
+const sequelize  = new Sequelize('safav3', 'root', '12345678', {
   host: '127.0.0.1',
   dialect: 'mysql',  
   define: {
